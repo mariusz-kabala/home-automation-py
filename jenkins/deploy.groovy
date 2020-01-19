42792b86eb5804695d630d42d57a25d99b050d6e
Mariusz <mariusz@kabala.waw.pl>
2020-01-19 21:14:42 +0100

    ci: add deploy jenkins job

---
+++ jenkins/deploy.groovy
@@ -0,0 +1,32 @@
+def branch = '';
+
+pipeline {
+    agent { docker { image 'ansible/ansible' } }
+    
+    environment {
+        CI = 'true'
+        GIT_SSH_COMMAND = "ssh -o StrictHostKeyChecking=no"
+        
+    }
+
+    stages {
+        stage ('Prepare') {
+            steps {
+                script {
+                    sh "printenv"
+                }
+            }
+        }
+        stage ('Deploy') {
+             steps {
+                script {
+                    sshagent(['jenkins-ssh-key']) {
+                        configFileProvider([configFile(fileId: 'homeAutomationPy-miio-config.py', targetLocation: 'config.py')]) {
+                            sh "ansible-playbook -i hosts deploy.yml -e 'app=${app} config_path=config.py'"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
