e3c384fbeaddcc06e42f64764c6036023c5b43b2
Mariusz <mariusz@kabala.waw.pl>
2020-01-19 21:54:19 +0100

    fix path

--- jenkins/deploy.groovy
+++ jenkins/deploy.groovy
@@ -22,7 +22,7 @@ pipeline {
                 script {
                     sshagent(['jenkins-local-ssh-key']) {
                         configFileProvider([configFile(fileId: 'homeAutomationPy-miio-config.py', targetLocation: 'config.py')]) {
-                            sh "ansible-playbook -i deploy/hosts deploy/deploy.yml -e 'app=${app} config_path=config.py'"
+                            sh "ansible-playbook -i deploy/hosts deploy/deploy.yml -e 'app=${app} config_path=config.py' -vvv"
                         }
                     }
                 }
