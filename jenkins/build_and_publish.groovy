220b4596ed1299cedc78fa862e0210e598e04504
Mariusz Kabała <mariusz@kabala.waw.pl>
2021-04-30 17:43:06 +0200

    testing

--- jenkins/build_and_publish.groovy
+++ jenkins/build_and_publish.groovy
@@ -56,9 +56,10 @@ pipeline {
             steps {
                 script {
                     dir("packages/${PACKAGE}") {
-                        sh "mkdir ${WORKSPACE_TMP}/tmp"
-                        sh "poetry config cache-dir ${WORKSPACE_TMP}/tmp/cache"
-                        sh "poetry config virtualenvs.path ${WORKSPACE_TMP}/tmp/virtualenvs"
+                        // sh "mkdir ${WORKSPACE_TMP}/tmp"
+                        // sh "poetry config cache-dir ${WORKSPACE_TMP}/tmp/cache"
+                        // sh "poetry config virtualenvs.path ${WORKSPACE_TMP}/tmp/virtualenvs"
+                        sh "RUN poetry config virtualenvs.create false"
                         sh "poetry config --list"
 
                         sh "poetry version ${VERSION}"
