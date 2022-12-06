5962de15f96b1a04a8a343637647da204546977d
Mariusz Kabała <mariusz@kabala.tech>
2022-12-06 21:10:36 +0100

    ci: jenkins build job adjustments

--- packages/lgsoundbar/jenkins/deploy.groovy
+++ packages/lgsoundbar/jenkins/deploy.groovy
@@ -57,70 +57,16 @@ pipeline {
                     ])
             }
         }
-        stage ('Build Apps') {
+        stage ('Build') {
             steps {
-                script {
-                    if (packages == "") {
-                        return
-                    }
-
-                    def packagesList = packages.split(',')
-
-                    packagesList.each {
-                        def props = readJSON file: "packages/app_${it}/package.json"
-                        def packageName = props['name'].replace('@', '').replace('-', '').toLowerCase()
-                        def currentApp = docker.build(packageName, "-f packages/app_${it}/Dockerfile .")
-
-                        manager.addShortText("${packageName}-${props['version']}", "white", "green", "1px", "navy")
-
+                dir("packages/lgsoundbar") {
+                    script {
                         docker.withRegistry('https://docker-registry.kabala.tech', 'docker-registry-credentials') {
-                            currentApp.push("v${props['version']}")
+                            sh "terraform init"
+                            sh "terraform plan -out deploy.plan -var=\"tag=${VERSION}\" -var=\"DOCKER_REGISTRY_USERNAME=${DOCKER_REGISTRY_USERNAME}\" -var=\"DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD}\"" 
+                            sh "terraform apply -auto-approve deploy.plan"
                         }
                     }
-                }
-            }
-        }
-        stage ('Build tasks') {
-            steps {
-                script {
-                    if (tasks == "") {
-                        return
-                    }
-
-                    def tasksList = tasks.split(',')
-
-                    tasksList.each {
-                        def props = readJSON file: "packages/task_${it}/package.json"
-                        def taskName = props['name'].replace('@', '').replace('-', '').toLowerCase()
-
-                        manager.addShortText("${taskName}-${props['version']}", "white", "green", "1px", "navy")
-
-                        def currentApp = docker.build(taskName, "-f packages/task_${it}/Dockerfile .")
-                        docker.withRegistry('https://docker-registry.kabala.tech', 'docker-registry-credentials') {
-                            currentApp.push("v${props['version']}")
-                            currentApp.push("latest")
-                        }
-                    }
-                }
-            }
-        }
-
-        stage ('Deploy') {
-            when {
-                environment name: 'deploy', value: 'true'
-            }
-            steps {
-                script {
-                        def packagesList = packages.split(',')
-                        packagesList.each {
-                            def props = readJSON file: "packages/app_${it}/package.json"
-
-                            build job: 'home/monorepo_deploy', wait: false, parameters: [
-                                string(name: 'ghprbActualCommit', value: "${ghprbActualCommit}"),
-                                string(name: 'package', value: "${it}"),
-                                string(name: 'version', value: "v${props.version}"),
-                            ]
-                        }
                 }
             }
         }
