e4ce2bf27859d6788628e776bdd9ce65bac93bff
Mariusz Kabała <mariusz@kabala.tech>
2022-12-06 19:17:22 +0100

    ci: jenkins jobs nad terraform

---
+++ packages/lgsoundbar/terraform/variables.tf
@@ -0,0 +1,26 @@
+variable "tag" {}
+
+
+variable "DOCKER_REGISTRY_USERNAME" {}
+
+variable "DOCKER_REGISTRY_PASSWORD" {}
+
+variable "DOCKER_REGISTRY" {
+    default = "docker-registry.kabala.tech"
+}
+
+variable "app_domain" {}
+
+variable "docker_host" {}
+
+variable "consul_host" {}
+
+variable "consul_port" {
+  default = "8500"
+}
+
+variable "network_name" {}
+
+variable "mqtt_host" {}
+
+variable "device_ip" {}
