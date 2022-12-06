c423b78cf5834d8793be40a065bb32e7c1c23667
Mariusz Kabała <mariusz@kabala.tech>
2022-12-06 21:17:21 +0100

    ci: jenkins build job adjustments

--- packages/lgsoundbar/jenkins/deploy.groovy
+++ packages/lgsoundbar/jenkins/deploy.groovy
@@ -45,7 +45,8 @@ pipeline {
                     script {
                         docker.withRegistry('https://docker-registry.kabala.tech', 'docker-registry-credentials') {
                             sh "terraform init"
-                            sh "terraform plan -out deploy.plan -var=\"tag=${VERSION}\" -var=\"DOCKER_REGISTRY_USERNAME=${DOCKER_REGISTRY_USERNAME}\" -var=\"DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD}\"" 
+                            sh "terraform workspace select ${env.DEPLOY_ENVIRONMENT} || terraform workspace new ${env.DEPLOY_ENVIRONMENT}"
+                            sh "terraform plan -out deploy.plan -var-file=${env.DEPLOY_ENVIRONMENT}.tfvars -var=\"tag=${version}\" -var=\"DOCKER_REGISTRY_USERNAME=${DOCKER_REGISTRY_USERNAME}\" -var=\"DOCKER_REGISTRY_PASSWORD=${DOCKER_REGISTRY_PASSWORD}\"" 
                             sh "terraform apply -auto-approve deploy.plan"
                         }
                     }
