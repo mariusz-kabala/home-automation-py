912e90bb5155053368f7317ef3a1be45e8eb1d60
Mariusz Kabała <mariusz@kabala.tech>
2022-12-12 14:56:50 +0100

    ci: drop traeffic label

--- packages/won/terraform/main.tf
+++ packages/won/terraform/main.tf
@@ -4,25 +4,6 @@ resource "docker_container" "won" {
   restart = "always"
   network_mode = "host"
 
-  # networks_advanced {
-  #     name = var.network_name
-  # }
-
-  labels {
-    label = "traefik.enable"
-    value = "true"
-  }
-
-  labels {
-    label = "traefik.http.routers.won.rule"
-    value = "Host(`${var.app_domain}`) && PathPrefix(`/${var.url_prefix}`)"
-  }
-
-  labels {
-    label = "traefik.http.services.won.loadbalancer.server.port"
-    value = "${var.http_port}"
-  }
-
   env = [
       "MQTT_HOST=${var.mqtt_host}",
       "HTTP_PORT=${var.http_port}",
